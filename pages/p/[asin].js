938f8635055a02fd301f3fc8bbc67b6ff97b090a
Bakkesh <bshivamurthy@kognivera.com>
2026-01-18 19:41:39 +0530

    Reslove Error  Netlify Blobs

--- pages/p/[asin].js
+++ pages/p/[asin].js
@@ -15,7 +15,8 @@ export async function getServerSideProps({ params, req }) {
 }
 
 export default function Product({ asin, country }) {
-  const image = `https://images-na.ssl-images-amazon.com/images/P/${asin}.01._SX300_.jpg`;
+  const image =
+    `https://images-na.ssl-images-amazon.com/images/P/${asin}.01._SX300_.jpg`;
 
   const domains = {
     IN: "amazon.in",
@@ -25,26 +26,34 @@ export default function Product({ asin, country }) {
 
   const domain = domains[country] || "amazon.in";
 
-  const amazon =
+  const amazonLink =
     `https://${domain}/dp/${asin}?tag=nbaffiliate0c-21`;
 
   return (
     <>
       <Head>
+        <title>View Product on Amazon</title>
         <meta property="og:title" content="View Product on Amazon" />
         <meta property="og:description" content="Check details and buy securely" />
         <meta property="og:image" content={image} />
       </Head>
 
+      {/* FIX: inject asin as string */}
       <script
         dangerouslySetInnerHTML={{
           __html: `
             fetch("/api/click", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ asin, country: "${country}" })
+              body: JSON.stringify({
+                asin: "${asin}",
+                country: "${country}"
+              })
             });
-            setTimeout(()=>{window.location.href="${amazon}"},500);
+
+            setTimeout(() => {
+              window.location.href = "${amazonLink}";
+            }, 500);
           `
         }}
       />
